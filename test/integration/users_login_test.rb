b31254a0e78622260fdf8fb9eb6c39635fdafe40
timwilki <tsw@tswdesign.co.uk>
2017-04-18 17:25:00 +0100

    add persistant sessions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,7 +10,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-    assert is_logged_in?                                      
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
