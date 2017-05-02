f55fe0b3ff81bf60fa2329162e502ac0d59a1e64
timwilki <tsw@tswdesign.co.uk>
2017-05-02 08:56:32 +0100

    add account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
