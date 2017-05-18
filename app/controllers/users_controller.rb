9c607fcf48cc16b78483203ed5dba3510d3f18de
timwilki <tsw@tswdesign.co.uk>
2017-05-18 12:12:40 +0100

    add microposts up to image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ before_action :admin_user, only: [:destroy]
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -52,20 +53,11 @@ before_action :admin_user, only: [:destroy]
 #everything beyond this point will be private, and not available to call on
   private
 
-    #before filters
-
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    #confirms a logged in user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "please log in"
-        redirect_to login_url
-      end
-    end
+    #before filters
 
     #confirms a correct user
     def correct_user
